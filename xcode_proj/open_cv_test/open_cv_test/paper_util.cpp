fe1b8b9ff60d8e2c0b5edf4ca70e08d72da953eb
Asger Pedersen <asger.pedersen@gmail.com>
2014-03-14 17:05:44 +0100

    Align cams

--- xcode_proj/open_cv_test/open_cv_test/paper_util.cpp
+++ xcode_proj/open_cv_test/open_cv_test/paper_util.cpp
@@ -10,6 +10,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/nonfree/features2d.hpp"
 
 #include <boost/filesystem.hpp>
@@ -144,6 +145,94 @@ void PaperUtil::foundMarker(vector< Point2f > marker_corners, vector<vector< Poi
     found.at(i) = (marker_corners);
 }
 
+
+Mat PaperUtil::alignCams(Mat logitech, Mat Kinect){
+    //-- Step 1: Detect the keypoints using SURF Detector
+    int minHessian = 400;
+    
+    SurfFeatureDetector detector( minHessian );
+    
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    
+    detector.detect( logitech, keypoints_object );
+    detector.detect( Kinect, keypoints_scene );
+    
+    //-- Step 2: Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
+    
+    Mat descriptors_object, descriptors_scene;
+    
+    extractor.compute( logitech, keypoints_object, descriptors_object );
+    extractor.compute( Kinect, keypoints_scene, descriptors_scene );
+    
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match( descriptors_object, descriptors_scene, matches );
+    
+    double max_dist = 0; double min_dist = 100;
+    
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+    
+    //printf("-- Max dist : %f \n", max_dist );
+    //printf("-- Min dist : %f \n", min_dist );
+    
+    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+    std::vector< DMatch > good_matches;
+    
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { if( matches[i].distance < 3*min_dist )
+    { good_matches.push_back( matches[i]); }
+    }
+    
+    Mat img_matches;
+    drawMatches( logitech, keypoints_object, Kinect, keypoints_scene,
+                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    
+    
+    //-- Localize the object from img_1 in img_2
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+    
+    for( size_t i = 0; i < good_matches.size(); i++ )
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+        scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+    }
+    
+    Mat H = findHomography( obj, scene, RANSAC );
+    
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = Point(0,0); obj_corners[1] = Point( logitech.cols, 0 );
+    obj_corners[2] = Point( logitech.cols, logitech.rows ); obj_corners[3] = Point( 0, logitech.rows );
+    std::vector<Point2f> scene_corners(4);
+    
+    perspectiveTransform( obj_corners, scene_corners, H);
+    
+    
+    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+    Point2f offset( (float)logitech.cols, 0);
+    line( img_matches, scene_corners[0] + offset, scene_corners[1] + offset, Scalar(0, 255, 0), 4 );
+    line( img_matches, scene_corners[1] + offset, scene_corners[2] + offset, Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[2] + offset, scene_corners[3] + offset, Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[3] + offset, scene_corners[0] + offset, Scalar( 0, 255, 0), 4 );
+    
+    //-- Show detected matches
+    imshow( "Good Matches & Object detection", img_matches );
+    
+    waitKey(0);
+    
+    return H;
+
+}
 
 // Goes through all found markers
 bool PaperUtil::touchedMarker(vector< vector<Point2f> > found, Point2f pt) {
--- xcode_proj/open_cv_test/open_cv_test/paper_util.h
+++ xcode_proj/open_cv_test/open_cv_test/paper_util.h
@@ -30,6 +30,7 @@ public:
     static bool checkAnglesInVector(vector<Point2f> v);
     static void foundMarker(vector<Point2f> marker_corners, vector< vector<Point2f> > found, size_t i);
     static bool touchedMarker(vector< vector<Point2f> > found, Point2f pt);
+    static Mat alignCams(Mat logitech, Mat Kinect);
 private:
     static double angle(Point2f pt1, Point2f pt2, Point2f pt0 );
     
